352df7cbb67f32eee28e261ffca1213b11e8372c
Daniil Dulin <daniildulin@gmail.com>
2019-01-23 17:21:21 +0300

    build 2019-01-23 17:21

---
+++ precache-manifest.8a11d5c9361734cb769c8ed2e930d8ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "068547b9a76d22ed4df5",
+    "url": "/static/css/main.341f1893.chunk.css"
+  },
+  {
+    "revision": "068547b9a76d22ed4df5",
+    "url": "/static/js/main.068547b9.chunk.js"
+  },
+  {
+    "revision": "a0b330815eb85b8f2921",
+    "url": "/static/js/1.a0b33081.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8ddedaaa8428ba6c9b38ffcbcc3da45",
+    "url": "/index.html"
+  }
+];
